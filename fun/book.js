7841c8a26c161bcabaec9b2e67e8106e46477e8b
treey-yao <949091796@qq.com>
2018-07-06 18:29:23 +0800

    解决循环异步这个恶心的东西

--- fun/book.js
+++ fun/book.js
@@ -15,61 +15,42 @@ var dir = './static/data/menu/sort01.json';
 // 爬取 电子书列表
 exports.booklist = function() {
 
-
     var book = [];
     var books = Array();
 
+    function indexid(i) {
+        superagent.get("https://www.qisuu.la/soft/sort01/index_" + i + ".html").end(function(err, res) {
+            var $ = cheerio.load(res.text);
+            var texts = $(".listBox");
 
-    var p = new Promise(function(resolve, reject) {
-
-        for (let i = 0; i < 11; i++) {
-            if (i == 0) {
-                superagent.get("https://www.qisuu.la/soft/sort01/").end(function(err, res) {
-                    var $ = cheerio.load(res.text);
-                    var texts = $(".listBox");
-
-                    // josn 电子书列表 内容
-                    for (let j = 0; j < texts.find("ul >li").length; j++) {
-                        var arr = '{' +
-                            '"bookId" : "A' + j + '",' +
-                            '"bookNmae" : "' + texts.find("ul >li").eq(j).find("a").eq(0).text() + '",' +
-                            '"bookImg" : "' + texts.find("ul >li").eq(j).find("img").attr("src") + '",' +
-                            '"bookInfo" : "' + texts.find("ul >li").eq(j).find(".s").text() + '",' +
-                            '"bookLink" : "' + texts.find("ul >li").eq(j).find("a").eq(0).attr("href") + '"' +
-                            '}'
-                        book.push(arr);
-                    }
-                })
+            // josn 电子书列表 内容
+            for (let k = 0; k < texts.find("ul >li").length; k++) {
+                var arr = '{' +
+                    '"bookId" : "A' + ((i * 14) + parseInt(k)) + '",' +
+                    '"bookNmae" : "' + texts.find("ul >li").eq(k).find("a").eq(0).text() + '",' +
+                    '"bookImg" : "' + texts.find("ul >li").eq(k).find("img").attr("src") + '",' +
+                    '"bookInfo" : "' + texts.find("ul >li").eq(k).find(".s").text() + '",' +
+                    '"bookLink" : "' + texts.find("ul >li").eq(k).find("a").eq(0).attr("href") + '"' +
+                    '}'
+                book.push(arr);
+            }
+            i++;
+            if (i < 10) {
+                indexid(i)
             } else {
-                superagent.get("https://www.qisuu.la/soft/sort01/index_" + i + ".html").end(function(err, res) {
-                    var $ = cheerio.load(res.text);
-                    var texts = $(".listBox");
 
-                    // josn 电子书列表 内容
-                    for (let k = 0; k < texts.find("ul >li").length; k++) {
-                        var arr = '{' +
-                            '"bookId" : "A' + (i * 14) + k + '",' +
-                            '"bookNmae" : "' + texts.find("ul >li").eq(k).find("a").eq(0).text() + '",' +
-                            '"bookImg" : "' + texts.find("ul >li").eq(k).find("img").attr("src") + '",' +
-                            '"bookInfo" : "' + texts.find("ul >li").eq(k).find(".s").text() + '",' +
-                            '"bookLink" : "' + texts.find("ul >li").eq(k).find("a").eq(0).attr("href") + '"' +
-                            '}'
-                        book.push(arr);
+                // josn 电子书列表 
+                books = '{"bookSort": [' + book + ']}'
+                fs.appendFile(dir, books, function(err) {
+                    if (err) {
+                        console.log(err);
                     }
-                })
+                    console.log("添加成功！");
+                });
+                return;
             }
-        }
-        resolve(book);
-    })
 
-    p.then(function(book) {
-        // josn 电子书列表 
-        books = '{"bookSort": [' + book + ']}'
-        fs.appendFile(dir, books, function(err) {
-            if (err) {
-                console.log(err);
-            }
-            console.log("添加成功！");
         });
-    })
+    }
+    indexid(2)
 }
